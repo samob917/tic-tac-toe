0ec047772505392bfcfd171556481c7c396f2164
Sam Oberly <samob917@outlook.com>
2025-03-10 23:42:42 -0400

    Adc checkBoard functionality
    Began adding checkboard functionality. This is not complete yet!
    the game is not correctly assessed to be over.

--- ticTacToe.js
+++ ticTacToe.js
@@ -6,6 +6,9 @@ have a Game factory which starts a game. It creates a game board
 
 function Gameboard() {
     let gameboard = [[0, 0, 0],[0, 0, 0],[0, 0, 0]];
+    let gameOver = false;
+    let winner = '';
+
     function makeMove(player, row, col) {
         if (gameboard[row][col] === 0) {
             gameboard[row][col] = player.marker;
@@ -14,8 +17,50 @@ function Gameboard() {
             console.log("Already Occupied!");
         }
     }
+
+    function checkArray(player, arr) {
+        if (arr.every(b => b === player.marker)) {
+            console.log(`${player.name} wins!`);
+            gameOver = true;
+            winner = player.name;
+        }
+    }
+    function checkRows(player) {
+        for (let i = 0; i<3; i++) {
+            let arr = gameboard[i];
+            checkArray(player, arr);
+        }
+    }   
+
+    function checkColumns(player) {
+        for (let i = 0; i < 3; i++) {
+            let arr = [gameboard[0][i], gameboard[1][i], gameboard[2][i]];
+            checkArray(player, arr)
+        }
+    }
+
+    function checkDiagonals(player) {
+        let diag1 = [gameboard[0][0], gameboard[1][1], gameboard[2][2]];
+        let diag2 = [gameboard[0][2], gameboard[1][1], gameboard[2][0]];
+        checkArray(diag1);
+        checkArray(diag2)
+    }
+
+    function checkBoard(players) {
+        for (player in players) {
+            checkColumns(player);
+            console.log()
+            checkRows(player);
+            checkDiagonals(player);
+            if (gameOver) {
+                return winner
+            }
+        }
+    }
     return {
-        makeMove
+        makeMove,
+        checkBoard,
+        gameOver
     }
 }
 
@@ -29,3 +74,4 @@ let player2 = new User("p2", "O")
 let game = new Gameboard();
 
 game.makeMove(player1, 0, 0);
+console.log(game.gameboard);
